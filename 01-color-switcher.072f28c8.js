1c74bc493a44e3218f5056784389aab5dda49023
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2023-01-12 15:33:42 +0000

    Deploying to gh-pages from @ Anzhelika-Light/goit-js-hw-09@74907ba561348d6fb9faad910b568a2af723971e 🚀

---
+++ 01-color-switcher.072f28c8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body,o=null;t.addEventListener("click",(function(){o=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(o)}))}();
+//# sourceMappingURL=01-color-switcher.072f28c8.js.map
