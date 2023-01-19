71500a84f126941c91b59a3285d416f888c27101
Anzhelika-Light <anzhelikaantoniuk1202@gmail.com>
2023-01-19 11:41:14 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,18 @@
+import Notiflix from 'notiflix';
+
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const bodyRef = document.body;
 
 let intervalId = null;
-console.log('Hello');
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 startBtn.addEventListener('click', () => {
-    btnStartEl.disabled = true;
-    btnStopEl.disabled = false;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
     intervalId = setInterval(() => {
         bodyRef.style.backgroundColor = getRandomHexColor();
         
@@ -20,8 +21,8 @@ startBtn.addEventListener('click', () => {
 
 stopBtn.addEventListener('click', () => {
     clearInterval(intervalId);
-    btnStartEl.disabled = false;
-    btnStopEl.disabled = true;
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
 
     Notiflix.Notify.info('Great choice!');
 })
